aea97bca50aedd4e08edaa49157694a6baddacaf
Toni Huang <toni@pluralagency.com.au>
2018-05-07 02:54:05 +1000

    added states level zooming and tweet positioning

--- src/app/heat-map/heat-map.service.ts
+++ src/app/heat-map/heat-map.service.ts
@@ -7,7 +7,7 @@ import * as topojson from "topojson-client";
 
 @Injectable()
 export class HeatMapService{
-    private url = 'http://hate-speech.local/topojson/world.json';
+    private mapDataUrl = 'http://hate-speech.local';
     private twitterDataServerUrl = 'http://139.59.110.130:5000';
     private socket;
 
@@ -16,28 +16,26 @@ export class HeatMapService{
     }
 
     getMapData(): Observable<any>{
-
-    
        //setTimeout(this._http.get('http://localhost:5000/'),5000);
-        return this._http.get(this.url)
+        return this._http.get(`${this.mapDataUrl}/topojson/world.json`)
             .map(this.extractData)
             .catch(this.handleError);
+    }
 
+    getStatesMapData(countryId:String): Promise<any> {
+        let statesMapDataPromise = this._http.get(`${this.mapDataUrl}/topojson/provinces/provinces_${countryId}.json`).toPromise();
+        return statesMapDataPromise;
     }
 
     getTwitterData(): Promise<any> {
         let twitterDataPromise = this._http.get(`${this.twitterDataServerUrl}/tweets`, { withCredentials: true }).toPromise();
-        twitterDataPromise.then(requestResult => {
-          // console.log(this.cart);
-          console.log(requestResult.json());
-        });
         return twitterDataPromise;
-      }
+    }
 
     private extractData(res: Response){
         let body=res.json();
         let countriesData = topojson.feature(body,body.objects.countries).features;
-        console.log(countriesData);
+        //console.log(body);
         return countriesData || {};
         //return body.data || {};
     }
